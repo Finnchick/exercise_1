3f1ab10bd5a934b5cfb186b64e7b6f4d059edc3f
siarheiklybik <sergey.klybik@1win.pro>
2024-09-04 18:33:18 +0200

    feat: added configModule and schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PageModule } from './page/page.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [PageModule],
+  imports: [
+    PageModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        KEY: Joi.string().required(),
+        PORT: Joi.number().port().default(3000),
+      }),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
