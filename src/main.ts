559108c8dee6bbb6b5ac3fb9b6e53c342c1728ba
siarheiklybik <sergey.klybik@1win.pro>
2024-09-04 18:37:08 +0200

    feat: added swagger, validation pipe, reworked dtos

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {AuthMiddleware} from "./auth/auth.middleware";
-import {SignMiddleware} from "./sign/sign.middleware";
-import {json} from "express";
+import { json } from 'express';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(json());
-  app.use(new AuthMiddleware().use, new SignMiddleware().use);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Pages example')
+    .setDescription('The pages API description')
+    .setVersion('1.0')
+    .addTag('pages')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
